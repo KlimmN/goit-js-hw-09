d3b794a332a90fa849252691797158eba1acec0a
KlimmN <klimnikita20022001@gmail.com>
2025-11-10 17:16:41 +0000

    Deploying to gh-pages from @ KlimmN/goit-js-hw-09@92f3bcbcdded585f0ddac0d571da36f0b16560dc 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector("form"),o="feedback-form-state";t.addEventListener("input",e=>{localStorage.setItem(o,e.target.value)});t.addEventListener("submit",e=>{e.preventDefault(),localStorage.removeItem(o),t.reset()});
+//# sourceMappingURL=2-form.js.map
